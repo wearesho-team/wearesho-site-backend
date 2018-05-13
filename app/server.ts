b5a08c4b7dce1523adff14533aea2192e7eb9ced
Alexander Letnikow <reclamme@gmail.com>
2018-05-13 14:51:02 +0300

    Fix importing meta

--- app/server.ts
+++ app/server.ts
@@ -10,7 +10,7 @@ import { CallbackController } from './controllers';
 const app: express.Application = express();
 export const port: number = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const meta = require("../../meta.json");
+const meta = require("../meta.json");
 
 app.use(require('cors')());
 app.use(require('body-parser').json({ type: '*/*' }));
